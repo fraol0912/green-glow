f9f986ececf57187c2bfeccd2e1182b8c94a0266
fraol0912 <fraol0912@gmail.com>
2020-12-30 02:50:13 +0300

    added styles to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,9 @@ import React from "react";
 // Component
 import SectionWithButton from "../components/SectionWithButton";
 
+// Styles
+import "../styles/index.scss";
+
 function NotFoundPage() {
   const buttonText = "Go To Home";
   const sectionText = "You have been Lost Again...";
