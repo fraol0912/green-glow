18ce90f44fad0d92fce967b7b54b2b1f59829a13
fraol0912 <fraol0912@gmail.com>
2021-01-03 05:34:08 +0300

    contact page preview

--- src/cms/preview-templates/ContactPagePreview.js
+++ src/cms/preview-templates/ContactPagePreview.js
@@ -5,9 +5,11 @@ import { ContactPageTemplate } from "../../templates/contact-page";
 
 function ContactPagePreview({ entry, getAsset }) {
   const data = entry.getIn(["data"]).toJS();
-  data.heroImage = getAsset(data.heroImage).url;
 
-  console.log(data);
+  data.heroImage = getAsset(data.heroImage).url;
+  data.facebookIcon = getAsset(data.facebookIcon).url;
+  data.linkedinIcon = getAsset(data.linkedinIcon).url;
+  data.telegramIcon = getAsset(data.telegramIcon).url;
 
   return (
     <>
